d83d6999e4fb3968eb398dc1b39b8be336c1a073
param-v <112659738+param-v@users.noreply.github.com>
2022-10-20 14:47:59 -0600

    arrayVisualizer

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hi");
+// Array Visualizer
+
+// HTML Variables
+let containerEl = document.getElementById("container");
+
+// Global Variable
+let myData = [400, 500, 250, 550, 100, 300, 600];
+
+// Display Data
+drawArray();
+
+function drawArray() {
+    let outputStr = "";
+    for (let i = 0; i < myData.length; i++) {
+        outputStr += `<div style="height: ${myData[i]}px">${myData[i]}</idv>`
+    }
+    containerEl.innerHTML = outputStr;
+}
+
