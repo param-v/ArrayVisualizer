d644ed007829d0ccf5fcad0805dbc1d62c270b50
param-v <112659738+param-v@users.noreply.github.com>
2022-10-27 12:21:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ let myData = [];
 for (let n = 1; n <= 50; n++) {
     myData.push(Math.random() * 600);
 }
-console.log(myData);
 
 // Display Data
 drawArray();
@@ -21,3 +20,30 @@ function drawArray() {
     containerEl.innerHTML = outputStr;
 }
 
+// Key Event Stuff
+document.addEventListener("keydown", keydownHandler)
+
+function keydownHandler(event) {
+    if (event.keyCode === 38) {
+        //Up Arrow - Add element to beginning
+        myData.splice(0, 0, Math.random() * 600);
+    } else if (event.keyCode === 40) {
+        // Down Arrow - Remove element from beginning
+        myData.splice(0, 1);
+        // The 0 in the brackets means remove a element to the beginning
+        // The 1 in brackets means remove 1 element
+    } 
+
+    // if (event.keyCode === 38) {
+    //     //Up Arrow - Push value to the end of the array
+    //     myData.push(Math.random() * 600);
+    // } else if (event.keyCode === 40) {
+    //     // Down Arrow - Remove value from the end of the array
+    //     let poppedValue = myData.pop();
+    //     console.log(poppedValue);
+    // } 
+    
+
+    // Redraw Array
+    drawArray();
+}
